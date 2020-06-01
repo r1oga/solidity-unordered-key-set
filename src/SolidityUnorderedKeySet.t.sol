f82aa4316193703d28d8446f9eb5fe2f21ea0049
r1oga <r1oga@tuta.io>
2020-06-01 21:29:35 +0200

    dapp init SolidityUnorderedKeySet

---
+++ src/SolidityUnorderedKeySet.t.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.5.15;
+
+import "ds-test/test.sol";
+
+import "./SolidityUnorderedKeySet.sol";
+
+contract SolidityUnorderedKeySetTest is DSTest {
+    SolidityUnorderedKeySet set;
+
+    function setUp() public {
+        set = new SolidityUnorderedKeySet();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
