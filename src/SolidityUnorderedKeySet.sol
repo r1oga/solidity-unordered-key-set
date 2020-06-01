2f4b2a2a3a69c343b33da0f330481e5a71837ff5
r1oga <r1oga@tuta.io>
2020-06-01 22:02:11 +0200

    Shorten error messages

--- src/SolidityUnorderedKeySet.sol
+++ src/SolidityUnorderedKeySet.sol
@@ -11,14 +11,14 @@ library SolidityUnorderedKeySetLib {
     }
 
     function insert(Set storage self, bytes32 key) internal {
-        require(key != 0x0, "UnorderedKeySet(100) - Key cannot be 0x0");
-        require(!exists(self, key), "UnorderedKeySet(101) - Key already exists in the set.");
+        require(key != 0x0, "Key cannot be 0x0");
+        require(!exists(self, key), "Key already exists in the set.");
         self.keyList.push(key);
         self.keyPointers[key] = len(self).sub(1);
     }
 
     function remove(Set storage self, bytes32 key) internal {
-        require(exists(self, key), "UnorderedKeySet(102) - Key does not exist in the set.");
+        require(exists(self, key), "Key does not exist in the set.");
         bytes32 keyToMove = self.keyList[len(self).sub(1)];
         uint rowToReplace = self.keyPointers[key];
         self.keyPointers[keyToMove] = rowToReplace;
@@ -44,6 +44,7 @@ library SolidityUnorderedKeySetLib {
         delete self.keyList;
     }
 }
+
 
 contract SolidityUnorderedKeySet {
     using SolidityUnorderedKeySetLib for SolidityUnorderedKeySetLib.Set;
