7011fe6d65b7cac875abbc6ae8b0c53cea3f6002
r1oga <r1oga@tuta.io>
2020-06-02 23:08:36 +0200

    test: Test len() and exists()

--- src/SolidityUnorderedKeySet.t.sol
+++ src/SolidityUnorderedKeySet.t.sol
@@ -1,21 +1,28 @@
 pragma solidity ^0.6;
-
 import "ds-test/test.sol";
-
 import "./SolidityUnorderedKeySet.sol";
 
+
 contract SolidityUnorderedKeySetTest is DSTest {
-    SolidityUnorderedKeySet set;
+    using SolidityUnorderedKeySetLib for SolidityUnorderedKeySetLib.Set;
+    SolidityUnorderedKeySetLib.Set set;
+    SolidityUnorderedKeySetLib.Set set2;
 
     function setUp() public {
-        set = new SolidityUnorderedKeySet();
+        // set = SolidityUnorderedKeySetLib.Set();
     }
 
-    function testFail_basic_sanity() public {
-        assertTrue(false);
+    function testLen() public {
+        assertEq(set.len(), 0);
+        set.keyList.push("test");
+        assertEq(set.len(), 1);
     }
 
-    function test_basic_sanity() public {
-        assertTrue(true);
+    function testExists() public {
+        assert(!set2.exists("test"));
+        set2.keyList.push("test");
+        set2.keyPointers["test"] = 0;
+        assert(set2.exists("test"));
+
     }
 }
